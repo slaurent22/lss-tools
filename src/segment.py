4c4c6f75e8532d2c920ba9c8c1cc3e5f208d9770
slaurent <slaurent22@pm.me>
2021-01-01 22:58:43 -0500

    construct Segment without comparison

--- src/segment.py
+++ src/segment.py
@@ -20,13 +20,6 @@ def get_time_iter_ms(segment_xml_root, min_attempt_id=None, comparison='GameTime
     return map(timeutil.to_milliseconds, parsed_times)
 
 
-def get_gold_time(segment_xml_root, comparison='GameTime'):
-    search_key = 'BestSegmentTime/{}'.format(comparison)
-    gold_string = segment_xml_root.find(search_key).text
-    time_tuple = timeutil.parse_time(gold_string)
-    return timeutil.to_milliseconds(time_tuple)
-
-
 def remove_outliers(times, mean, deviation):  # Calc Z Score and remove if outlier
     n = 0
     while n in range(len(times)):
@@ -40,17 +33,21 @@ def remove_outliers(times, mean, deviation):  # Calc Z Score and remove if outli
 
 class Segment:
 
-    def __init__(self, xml_root, comparison='GameTime'):
-        self.comparison = comparison
+    def __init__(self, xml_root):
         self.name = xml_root.find('Name').text
-        self.gold = get_gold_time(xml_root, comparison)
         self.__xml_root__ = xml_root
 
-    def get_stats(self, min_attempt_id=None):
+    def get_gold_time(self, comparison='GameTime'):
+        search_key = 'BestSegmentTime/{}'.format(comparison)
+        gold_string = self.__xml_root__.find(search_key).text
+        time_tuple = timeutil.parse_time(gold_string)
+        return timeutil.to_milliseconds(time_tuple)
+
+    def get_stats(self, min_attempt_id=None, comparison='GameTime'):
         times = list(get_time_iter_ms(self.__xml_root__,
-                                      comparison=self.comparison, min_attempt_id=min_attempt_id))
+                                      comparison=comparison, min_attempt_id=min_attempt_id))
         mean = int(statistics.mean(times))
         median = int(statistics.median(times))
         deviation = int(statistics.stdev(times))
         mean, median, deviation = remove_outliers(times, mean, deviation)
-        return mean, deviation, self.name, median, self.gold
+        return mean, deviation, self.name, median, self.get_gold_time(comparison=comparison)
--- src/splits.py
+++ src/splits.py
@@ -10,9 +10,10 @@ def deviation_key(stats):
 class Splits:
 
     def __init__(self, splits_xml_root):
-        self.__segments_xml_root__ = splits_xml_root.find('Segments')
+        segments_xml_root = splits_xml_root.find('Segments')
+        self.segments = map(lambda segment_xml_root: Segment(segment_xml_root), segments_xml_root)
 
     def get_deviations(self, min_attempt_id=None, comparison='GameTime'):
-        segments = map(lambda segment_xml_root: Segment(segment_xml_root, comparison), self.__segments_xml_root__)
-        stats = map(lambda segment: segment.get_stats(min_attempt_id=min_attempt_id), segments)
+        stats = map(lambda segment: segment.get_stats(
+            min_attempt_id=min_attempt_id, comparison=comparison), self.segments)
         return sorted(stats, reverse=True, key=deviation_key)
